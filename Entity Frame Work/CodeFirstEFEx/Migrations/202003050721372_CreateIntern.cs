5378b6135e3ec5465fd59c37f4777cbe6bc0feab
Yaseerarfath <47584773+Yaseerarfath@users.noreply.github.com>
2020-04-26 12:32:55 +0530

    Add files via upload

---
+++ Entity Frame Work/CodeFirstEFEx/Migrations/202003050721372_CreateIntern.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeFirstEFEx.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateIntern : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
